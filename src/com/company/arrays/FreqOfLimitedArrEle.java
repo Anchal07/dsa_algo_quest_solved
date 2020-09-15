cc2d69c25beddc1693fc07e5cfefb97bce3e7515
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-09-15 22:16:20 +0530

    ..

--- src/com/company/arrays/FreqOfLimitedArrEle.java
+++ src/com/company/arrays/FreqOfLimitedArrEle.java
@@ -12,9 +12,27 @@ public class FreqOfLimitedArrEle {
         for(int i=0; i<n; i++)
             arr[i]=scanner.nextInt();
         Arrays.stream(arr).sorted();
+        int counter=arr[0], freq=1;
         for(int i=0; i<n;){
-            
+            if((arr[i]!=0 || arr[i]-arr[i-1]-1==counter) && counter>0){
+                if(counter!=1){
+                    System.out.println(0);
+                    counter--;
+                }
+            } else{
+                if(arr[i] == arr[i+1]){
+                    freq++;
+                    i++;
+                }
+                else if(i+1<n && arr[i] != arr[i+1]){
+                    System.out.println(freq);
+                    if(arr[i+1]-arr[i]==1)
+                        freq=1;
+                    else
+                        counter=arr[i+1]-arr[i]-1;
+                    i++;
+                }
+            }
         }
     }
-
 }
