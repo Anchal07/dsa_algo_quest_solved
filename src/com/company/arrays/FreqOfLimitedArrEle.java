c91f9d25bd32c2c001c82e3f99cc3568b1996df5
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-09-17 08:45:04 +0530

    fre of limited arr ele with o(1) space

--- src/com/company/arrays/FreqOfLimitedArrEle.java
+++ src/com/company/arrays/FreqOfLimitedArrEle.java
@@ -12,14 +12,18 @@ public class FreqOfLimitedArrEle {
         for(int i=0; i<n; i++)
             arr[i]=scanner.nextInt();
         Arrays.stream(arr).sorted();
-        int counter=arr[0], freq=1;
+        int counter=arr[0], freq=1, k=counter;
         for(int i=0; i<n;){
-            if((arr[i]!=0 || arr[i]-arr[i-1]-1==counter) && counter>0){
+            if((arr[i]!=0 || (i-1 >0 && arr[i]-arr[i-1]-1==k)) && counter>1){
                 if(counter!=1){
                     System.out.println(0);
                     counter--;
                 }
             } else{
+                if(i==n-1 && arr[i]-arr[i-1]!=1){
+                    System.out.println(freq);
+                    break;
+                }
                 if(arr[i] == arr[i+1]){
                     freq++;
                     i++;
@@ -28,8 +32,11 @@ public class FreqOfLimitedArrEle {
                     System.out.println(freq);
                     if(arr[i+1]-arr[i]==1)
                         freq=1;
-                    else
-                        counter=arr[i+1]-arr[i]-1;
+                    else{
+                        freq=1;
+                        counter=arr[i+1]-arr[i];
+                        k=counter;
+                    }
                     i++;
                 }
             }
