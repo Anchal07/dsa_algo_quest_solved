0fc74666b6a35a9068a621c485e672aedb7b162e
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-14 23:25:35 +0530

    3.Binary Search - Index of 1st occurrence of number in sorted array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
-import com.company.arrays.*;
+import com.company.BinarySearch.FirstOccurenceOfNum;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new LongestSubArrayOfEvenOdds().longestSubarray();
+        new FirstOccurenceOfNum().binarySearchFirstOccNum();
     }
 }
