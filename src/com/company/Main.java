5eb50c8b8e6b814124e823d48972478b94411d61
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-15 22:53:57 +0530

    1.last occurence 2.square root

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
 import com.company.BinarySearch.FirstOccurenceOfNum;
+import com.company.BinarySearch.LastOccurrenceOfNum;
+import com.company.BinarySearch.SquareRootOfNum;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new FirstOccurenceOfNum().binarySearchFirstOccNum();
+        new SquareRootOfNum().sqRoot();
     }
 }
