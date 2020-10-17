ad0a6095eb3bd5de72fdafe000876a43c1897fea
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-17 21:53:56 +0530

    2. minimum number in sorted rotated array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import com.company.BinarySearch.FloorInSortedArray;
+import com.company.BinarySearch.MiniInSortedRotatedArray;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new FloorInSortedArray().floor();
+        new MiniInSortedRotatedArray().minimum();
     }
 }
