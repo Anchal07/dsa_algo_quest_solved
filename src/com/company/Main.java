6644083b33eb03854da7a8464c454daa00f7d8f6
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-22 22:30:49 +0530

    2. insertion sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,12 @@
 package com.company;
 
-import com.company.sorting.BubbleSortInO_n;
-import com.company.sorting.IntersectionOf2Arrays;
-import com.company.sorting.UnionOf2Arrays;
+import com.company.sorting.InsertionSort;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new BubbleSortInO_n().sort();
+        new InsertionSort().sort();
     }
 }
