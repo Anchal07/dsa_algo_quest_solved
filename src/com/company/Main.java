1cfb989335af00d07cb1eed0343b055f6adda829
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-24 23:21:31 +0530

    2.1 inversoin of array - status  :half code completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import com.company.sorting.BinaryArraySorting;
+import com.company.sorting.InversionOfArray;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new BinaryArraySorting().sort();
+        new InversionOfArray().inversion();
     }
 }
