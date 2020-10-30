12fc76a0b9197aa20650dd1c7e253a2ba1fb02a5
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-30 22:49:37 +0530

    1. completed minimum platforms

--- src/com/company/sorting/MinimumPlatforms.java
+++ src/com/company/sorting/MinimumPlatforms.java
@@ -1,5 +1,8 @@
 package com.company.sorting;
 
+import jdk.internal.util.xml.impl.Pair;
+
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class MinimumPlatforms {
@@ -14,45 +17,18 @@ public class MinimumPlatforms {
             arrival[i] = scanner.nextInt();
         for(int i=0; i<n; i++)
             departure[i] = scanner.nextInt();
-        int platforms=0;
-        sortDepartureWise(arrival, departure, 0, m-1);
-    }
-
-    void sortDepartureWise(int arr[], int dept[], int l, int h){
-        if(l<h){
-            int m = l + (h-l)/2;
-            sortDepartureWise(arr, dept, l, h);
-            sortDepartureWise(arr, dept, l, h);
-            sort(arr, dept, l, h, m);
-        }
-    }
-
-    void sort(int arr[], int dept[], int l, int h, int m){
-        int n1 = m-l+1;
-        int n2 = h-m;
-        int left[] = new int[n1];
-        int right[] = new int[n2];
-        for(int i=0; i<n1; i++)
-            left[i] = dept[l+i];
-        for(int i=0; i<n2; i++)
-            right[i] = dept[m+i+1];
-        int k=0;
-        for(int i=0, j=0; i<n1 && j<n2; ){
-            if(left[i]<right[j]){
-                dept[k++] = left[i];
-                swap(arr, k, i);
+        int platforms=0, max = 0;
+        for(int i=0, j=0; i<n && j<m; ){
+            if(arrival[i]<departure[j]){
+                platforms++;
                 i++;
+            } else if(arrival[i] > departure[j]){
+                platforms--;
+                j++;
             }
-            if(left[i]>dept[j]){
-
-            }
+            if(platforms > max)
+                max = platforms;
         }
+        System.out.println(max+1);
     }
-
-    void swap(int arr[], int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[i] = temp;
-    }
-
 }
