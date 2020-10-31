c52021466e06c41e5e2945c1e315dc6b17698830
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-10-31 21:46:01 +0530

    1. snake pattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
-import com.company.sorting.*;
+import com.company.matrix.SnakePattern;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new MinimumPlatforms().platforms();
+        new SnakePattern().pattern();
     }
 }
