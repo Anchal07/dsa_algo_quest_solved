a2a477feac918c67525970f4cb8891e7cd356ea3
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-11-10 21:51:36 +0530

    1. chaining

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,12 @@
 package com.company;
 
-import com.company.matrix.SpiralTraversal;
+import com.company.hashing.Chaining;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new SpiralTraversal().traversal();
+        new Chaining().hashByChain();
     }
 }
