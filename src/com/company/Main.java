ab78ad916f666a6337e5f8e9f5341c4fe8a6200b
anchal_07 <anchal.chaudhary@blu-smart.com>
2020-11-10 22:40:45 +0530

    2. subarray with 0 sum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,13 @@
 package com.company;
 
 import com.company.hashing.Chaining;
+import com.company.hashing.SubarrayWith0Sum;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hi");
-        new Chaining().hashByChain();
+        new SubarrayWith0Sum().subArray();
     }
 }
